8dadb1cdca099c7b249ea13d503a694954dbcef3
Marcos Alberto Sanmartín Pereira <ahriman89@gmail.com>
2024-11-07 22:15:15 +0100

    Insertar por lotes con promesas

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
 import axios, { AxiosInstance } from 'axios';
+
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
@@ -7,21 +12,34 @@ export class SeedService {
 
   private readonly axios: AxiosInstance = axios;
 
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+
+  ) {}
+
   async executeSeed() {
 
+    await this.pokemonModel.deleteMany({});
+
     const { data } = await axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10');
+
+    const insertPromisesAray= [];
 
     data.results.forEach(({ name, url}) => {
 
       const segments = url.split('/');
       const no: number = +segments[segments.length - 2];
 
-      console.log({ name, no });
+      // const pokemon = await this.pokemonModel.create({ name, no });
+
+      insertPromisesAray.push( this.pokemonModel.create({ name, no }) );
 
     });
 
+    const newArray = await Promise.all( insertPromisesAray );
 
-    return data.results;
+    return 'Seed executed';
   }
 
 }
